a205d023d84769822760ffa5dce79fe1b1e266c5
drotsyk <43176477+drotsyk@users.noreply.github.com>
2021-04-27 19:40:07 +0300

    Delete src directory

--- src/request.js
+++
@@ -1,9 +0,0 @@
-export const request = () => {
-    return fetch('https://swapi.dev/api/people/')
-    .then(res => {
-        if(!res.ok){
-            throw new Error(`${res.status} - ${res.statusText}`)
-        }
-        return res.json()
-    })
-}
